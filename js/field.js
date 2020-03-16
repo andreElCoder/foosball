89e47d71fccc49e3eb6c9dafd975cf064d05fd02
Andre R <andre.rabaca94@gmail.com>
2020-03-16 12:44:04 +0000

    Only added Game Class and main.js

--- js/field.js
+++ js/field.js
@@ -1,3 +1,13 @@
-class Field(){
+class Field{
+    constructor(game){
+    this.game=game
+    this.ctx= game.ctx
+}
 
+displayField(){
+
+    this.ctx.beginPath();
+    this.ctx.fillStyle="green";
+    this.ctx.fillRect(0,0,this.game.width,this.game.height)
+}
 }
--- js/game.js
+++ js/game.js
@@ -2,6 +2,8 @@ class Game {
     constructor(canvas){
     this.$canvas=canvas
     this.ctx = document.querySelector("canvas")
+    this.width = this.$canvas.width;
+    this.height= this.$canvas.height;
     this.field=new Field(this)
     this.playerArray=[];
     this.ball = new Ball(this);
