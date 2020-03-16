72c11230af687b442144ccbc7d8063d991c74786
Andre R <andre.rabaca94@gmail.com>
2020-03-16 12:23:34 +0000

    canvas and style . css added

--- js/game.js
+++ js/game.js
@@ -2,6 +2,8 @@ class Game {
     constructor(canvas){
     this.$canvas=canvas
     this.ctx = document.querySelector("canvas")
-    
+    this.field=new Field(this)
+    this.playerArray=[];
+    this.ball = new Ball(this);
     }
 }
